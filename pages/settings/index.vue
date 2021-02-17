b3de095520eb02a352dadf35b0d19437aeba14d0
pflqq <8341102+pflqq@user.noreply.gitee.com>
2021-02-17 21:56:14 +0800

    init

---
+++ pages/settings/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="settings-page">
+    <div class="container page">
+      <div class="row">
+
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Your Settings</h1>
+
+          <form>
+            <fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control" type="text" placeholder="URL of profile picture">
+                </fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+                </fieldset>
+                <fieldset class="form-group">
+                  <textarea class="form-control form-control-lg" rows="8" placeholder="Short bio about you"></textarea>
+                </fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control form-control-lg" type="text" placeholder="Email">
+                </fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control form-control-lg" type="password" placeholder="Password">
+                </fieldset>
+                <button class="btn btn-lg btn-primary pull-xs-right">
+                  Update Settings
+                </button>
+                <hr style="margin-top:85px"/>
+                <button class="btn btn-outline-danger" @click.prevent="onLogout">
+                  Or click here to logout.
+                </button>
+            </fieldset>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 仅在客户端加载 js-cookie 包
+const Cookie = process.client ? require('js-cookie') : undefined
+export default {
+  middleware: 'authenticated',
+  name: 'SettingsIndex',
+  methods:{
+    onLogout(){
+      this.$store.commit('setUser', '')
+      Cookie.set('user', '')
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
